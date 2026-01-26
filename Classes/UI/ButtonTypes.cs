972e692c05c6438828e07fa8939a54ff3ef5f7ed
Logan <logan.panucat2@gmail.com>
2026-01-26 16:01:37 +0800

    Add initial Blazor UI components and project setup
    Introduces core UI components (Button, Form, FormField, FormInput, FormMessage) and supporting types for a modern Blazor UI library inspired by shadcn/ui. Sets up the project file, solution, and required dependencies. Includes example static assets and import directives.

---
+++ Classes/UI/ButtonTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace GANBlazor.UI.Classes.UI
+{
+    public class ButtonTypes
+    {
+        public enum ButtonVariant { Default, Outline, Ghost, Danger }
+        public enum ButtonSize { Sm, Md, Lg }
+        public enum ButtonAlign { Start, Center, End }
+        public enum ButtonType { Button, Submit, Reset }
+    }
+}
